9ef080e34e5497c3a3f9eeaf51bbad6b72e1c6d8
EvolutionX <jeyush2005@gmail.com>
2022-07-29 09:12:38 +0530

    style: pretty

--- TypeScript/permCheck.ts
+++ TypeScript/permCheck.ts
@@ -20,26 +20,26 @@
 import { type GuildMember, PermissionResolvable } from "discord.js";
 import { CommandType, EventPlugin, PluginType } from "@sern/handler";
 export function permCheck(
-  perm: PermissionResolvable,
-  response: string
+	perm: PermissionResolvable,
+	response: string
 ): EventPlugin<CommandType.Both> {
-  return {
-    type: PluginType.Event,
-    description: "Checks for specified perm",
-    async execute(event, controller) {
-      const [ctx] = event;
-      if (ctx.guild === null) {
-        ctx.reply("This command cannot be used here");
-        console.warn(
-          "PermCheck > A command stopped because we couldn't check a users permissions (was used in dms)"
-        ); //delete this line if you dont want to be notified when a command is used outside of a guild/server
-        return controller.stop();
-      }
-      if (!(ctx.member! as GuildMember).permissions.has(perm)) {
-        await ctx.reply(response);
-        return controller.stop();
-      }
-      return controller.next();
-    },
-  };
+	return {
+		type: PluginType.Event,
+		description: "Checks for specified perm",
+		async execute(event, controller) {
+			const [ctx] = event;
+			if (ctx.guild === null) {
+				ctx.reply("This command cannot be used here");
+				console.warn(
+					"PermCheck > A command stopped because we couldn't check a users permissions (was used in dms)"
+				); //delete this line if you dont want to be notified when a command is used outside of a guild/server
+				return controller.stop();
+			}
+			if (!(ctx.member! as GuildMember).permissions.has(perm)) {
+				await ctx.reply(response);
+				return controller.stop();
+			}
+			return controller.next();
+		},
+	};
 }
