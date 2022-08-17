498e08f722c2b95ecf1e5b36dbce06784cfb08a2
Neo <80315475+NeoYaBoi@users.noreply.github.com>
2022-08-18 09:53:14 +1000

    fix: Forgot to format with prettier

--- TypeScript/channelType.ts
+++ TypeScript/channelType.ts
@@ -17,28 +17,27 @@
  * })
  * ```
  */
- import {
-	ChannelType
-} from "discord.js";
+import { ChannelType } from "discord.js";
 import { CommandType, EventPlugin, PluginType } from "@sern/handler";
 export function channelType(
-    channelType: ChannelType, 
-	onFail: string
+  channelType: ChannelType,
+  onFail: string
 ): EventPlugin<CommandType.Both> {
-	return {
-		type: PluginType.Event,
-		description: "Checks the channel type.",
-		async execute(event, controller) {
-			const [ctx] = event;
-			if(ctx.guild === null) { //change this to isDMBased if you want. (don't work for me)
-				await ctx.reply(onFail)
-				return controller.stop()
-			}
-			if(channelType !== ctx.channel.type) {
-                await ctx.reply(onFail)
-                return controller.stop()
-            }
-			return controller.next()
-		},
-	};
+  return {
+    type: PluginType.Event,
+    description: "Checks the channel type.",
+    async execute(event, controller) {
+      const [ctx] = event;
+      if (ctx.guild === null) {
+        //change this to isDMBased if you want. (don't work for me)
+        await ctx.reply(onFail);
+        return controller.stop();
+      }
+      if (channelType !== ctx.channel.type) {
+        await ctx.reply(onFail);
+        return controller.stop();
+      }
+      return controller.next();
+    },
+  };
 }
