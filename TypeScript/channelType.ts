15319459a87da54b4a52296f9eb19b61af66560b
Neo <80315475+NeoYaBoi@users.noreply.github.com>
2022-08-18 11:46:51 +1000

    fix: fixed requested changes
    regarding optional onFail: I wasn't sure if it would throw an error due to the plugin attempting to send a empty message so I put it in a check.

--- TypeScript/channelType.ts
+++ TypeScript/channelType.ts
@@ -10,31 +10,28 @@
  * import { channelType } from "discord.js"
  * import { commandModule } from "@sern/handler";
  * export default commandModule({
- *  plugins: [ channelType(channelType.GuildText, 'This cannot be used here') ],
+ *  plugins: [ channelType(ChannelType.GuildText, 'This cannot be used here') ],
  *  execute: (ctx) => {
  * 		//your code here
  *  }
  * })
  * ```
  */
-import { ChannelType } from "discord.js";
+import type { ChannelType } from "discord.js";
 import { CommandType, EventPlugin, PluginType } from "@sern/handler";
 export function channelType(
   channelType: ChannelType,
-  onFail: string
+  onFail?: string
 ): EventPlugin<CommandType.Both> {
   return {
     type: PluginType.Event,
     description: "Checks the channel type.",
     async execute(event, controller) {
       const [ctx] = event;
-      if (ctx.guild === null) {
-        //change this to isDMBased if you want. (don't work for me)
-        await ctx.reply(onFail);
-        return controller.stop();
-      }
-      if (channelType !== ctx.channel.type) {
-        await ctx.reply(onFail);
+      if (channelType !== ctx.channel?.type) {
+        if (onFail) {
+          await ctx.reply(onFail);
+        }
         return controller.stop();
       }
       return controller.next();
