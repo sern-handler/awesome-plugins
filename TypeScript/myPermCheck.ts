87483eda2e68bc4130203c105089680c67f5c9c8
Gary <garyneedham1@gmail.com>
2022-11-25 08:43:17 -0600

    refactor: made requested changes to feat pr.

--- TypeScript/myPermCheck.ts
+++ TypeScript/myPermCheck.ts
@@ -2,15 +2,15 @@
 /**
  * This is perm check, it allows users to parse the permission you want and let the plugin do the rest. (check bot or user for that perm).
  *
- * @author @NeoYaBoi [<@762918086349029386>]
-   @author @needhamgary [<@342314924804014081]
+ * @author @Benzo-Fury [<@762918086349029386>]
+ * @author @needhamgary [<@342314924804014081>]
  * @version 1.1.0
  * @example
  * ```ts
- * import { permCheck } from "../plugins/myPermCheck";
+ * import { requirePermission } from "../plugins/permCheck";
  * import { commandModule } from "@sern/handler";
  * export default commandModule({
- *  plugins: [ permCheck('bot or user', 'permission', 'No permission response') ],
+ *  plugins: [ requirePermission('target', 'permission', 'No permission response') ],
  *  execute: (ctx) => {
  * 		//your code here
  *  }
@@ -18,16 +18,27 @@
  * ```
  */
 
-import { type GuildMember, PermissionResolvable } from "discord.js";
+import {
+  type GuildMember,
+  PermissionResolvable,
+  GuildMemberResolvable,
+} from "discord.js";
 import { CommandType, EventPlugin, PluginType } from "@sern/handler";
-export function permCheck(
-  who: string,
+export function requirePermission(
+  target: GuildMemberResolvable,
   perm: PermissionResolvable,
-  response: string
-): EventPlugin<CommandType.Both> {
+  response?: string
+): EventPlugin<
+  CommandType.Both,
+  CommandType.MenuMsg,
+  CommandType.MenuSelect,
+  CommandType.MenuUser,
+  CommandType.Modal,
+  CommandType.Button
+> {
   return {
     type: PluginType.Event,
-    description: "Checks bot perms",
+    description: "Checks bot/user perms",
     async execute(event, controller) {
       const [ctx] = event;
       if (ctx.guild === null) {
@@ -37,22 +48,33 @@ export function permCheck(
         ); //delete this line if you dont want to be notified when a command is used outside of a guild/server
         return controller.stop();
       }
-      switch (who) {
+      switch (target) {
         case "bot":
-          let mem = ctx.guild.members.me;
-          if (!(mem! as GuildMember).permissions.has(perm)) {
+          if (
+            !(
+              (await ctx.guild.members.fetchMe({
+                cache: false,
+              })!) as GuildMember
+            ).permissions.has(perm)
+          ) {
+            if (!response)
+              response = `I cannot use this command, please give me \`${perm}\` permission.`;
             await ctx.reply(response);
             return controller.stop();
           }
           return controller.next();
         case "user":
           if (!(ctx.member! as GuildMember).permissions.has(perm)) {
+            if (!response)
+              response = `You cannot use this command because you are missing \`${perm}\` permission.`;
             await ctx.reply(response);
             return controller.stop();
           }
           return controller.next();
         default:
-          break;
+          console.warn("Perm Check >>> You didn't specify user or bot.");
+          ctx.reply("User or Bot was not specified.");
+          return controller.stop();
       }
     },
   };
