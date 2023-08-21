f6c11aa789b5175d27baa9cbb21f617ca05a2ff9
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-08-21 00:32:49 -0500

    fix for multiple authors

--- scripts/gen-list.clj
+++ scripts/gen-list.clj
@@ -46,29 +46,33 @@
     :else {:unknown line}
   ))
 
-(defn get-description [file-data]
+(defn get-description [file-data, kyword]
   "description will be the first section of unknown lines, up until an author tag"
-  (split-with (comp not :author) file-data))
+  (split-with (comp not kyword) file-data))
 
 (defn combine-lines [acc cur]
     (str acc "\n" cur))
 
-
+(defn accumulate-multiple [acc cur] 
+  (let [authors (:author cur)]
+    (conj acc cur)))
+  
 (defn file-data [plugin-name]
   "gets all jsdoc content. Transforms into lazy seq of data"
   (let [file (io/file (str "./TypeScript/" plugin-name))
         lines (->> (read-lines file) 
                    into-jsdoc-content
                    (map group-doc))
-        [description, 
-         [author version example & remaining]] (get-description lines)]
-        [[:description (reduce combine-lines "" (map :unknown description))]
-         [:version (:version version)]
-         [:author (:author author)]
-         [:example (reduce combine-lines "" (map :unknown remaining))]]
+        [description, remaining] (get-description lines :author)
+        [authors, [version, & remaining2]] (get-description remaining :version)]
+        {:description (reduce combine-lines "" (map :unknown description))
+         :version (:version version)
+         :author (reduce accumulate-multiple [] (map :author authors))
+         :example (reduce combine-lines "" (map :unknown remaining2))}
     ))
 
-(defn get-filename [file] (.getName file))
+(defn get-filename [file] 
+  (.getName file))
 
 (defn copy+md5 [source sink]
   (let [digest (java.security.MessageDigest/getInstance "MD5")]
@@ -87,7 +91,7 @@
           (let [ 
                 fname (get-filename file)
                 file-data (file-data fname)] 
-            (into (hash-map 
+            (conj (hash-map 
                     :name (name-without-extension fname) 
                     :link (make-link fname)
                     :hash (copy+md5 file (java.io.OutputStream/nullOutputStream)))
