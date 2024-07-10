34af2ab405c018a8fe596c6cc1038e6365ddfc8d
Peter-MJ-Parker <34216187+Peter-MJ-Parker@users.noreply.github.com>
2024-07-09 21:57:20 -0500

    edit: resolve conflicts
    Removed deconstructed interaction
    Made errors more understandable
    Corrected the example to use a parameter as the first object and input correct properties.

--- plugins/subcommandPermCheck.ts
+++ plugins/subcommandPermCheck.ts
@@ -7,20 +7,20 @@
  * @version 1.0
  * @example
  * ```ts
- * import { subcommandPermCheck } from "../plugins/subcommandPerms";
+ * import { subcommandPermCheck } from "../plugins/subcommandPerms.js";
  * import { commandModule, CommandType } from "@sern/handler";
  * import { PermissionFlagBits } from "discord.js";
  * export default commandModule({
  *  type: CommandType.Slash,
  *  plugins: [
- *   subcommandPermCheck(
- *    [
+ *   subcommandPermCheck({
+ *    list: [
  *      { name: "string", perms: [PermissionFlagBits.Administrator] },
  *      { name: "number", perms: [PermissionFlagBits.SendMessages, PermissionFlagBits.UseVAD] }
  *    ],
- *   all: true, //all = Require the member to have all perms stated or at least one?
- *   "OPTIONAL - respond to user with this message or default."
- *   )
+ *   needAllPerms: true, //all = Require the member to have all perms stated or at least one?
+ *   //response: "OPTIONAL - respond to user with this message or default."
+ *   })
  *  ],
  *  execute: ({ interaction }) => {
  * 		//your code here
@@ -31,7 +31,7 @@
  */
 
 /** Marked TODO's will be reconfigured with release of sern v4! **/
-import { type GuildMember, type PermissionResolvable, type TextChannel } from 'discord.js';
+import type { GuildMember, PermissionResolvable, TextChannel } from 'discord.js';
 import { type CommandType, CommandControlPlugin, controller } from '@sern/handler';
 
 export const permsToString = (...perms: PermissionResolvable[]) => {
@@ -42,40 +42,39 @@ export const permsToString = (...perms: PermissionResolvable[]) => {
 };
 
 export function subcommandPermCheck(opts: Options) {
-  return CommandControlPlugin<CommandType.Slash>(async (ctx, [, args]) => {
+  return CommandControlPlugin<CommandType.Slash>(async (ctx) => {
     if (!ctx.isSlash()) {
-      throw new Error('You provided a Text command.', { cause: "The plugin 'subcommandPermCheck' is meant for Slash commands only!" });
+      throw new Error('You did not provide a slash command.', { cause: "The plugin 'subcommandPermCheck' is meant for Slash commands only!" });
       //TODO: return state to command rather than error
       return controller.stop();
     }
 
-    const { interaction } = ctx;
-    if (interaction.guild === null) {
-      await interaction.reply({
+    if (ctx.guild === null) {
+      await ctx.reply({
         content: "This sub command cannot be used in DM's!",
         ephemeral: true
       });
       return controller.stop();
     }
-    const member = interaction.member as GuildMember;
+    const member = ctx.member as GuildMember;
     const subcommands = opts.list;
-    let sub = args.getSubcommand();
+    let sub = ctx.options.getSubcommand();
 
     /** WILL BE REWRITTEN WHEN SERN V4 IS RELEASED!!! **/
     for (const { name, perms } of subcommands) {
       if (name !== sub) {
         throw new Error("You provided a subcommand name which doesn't exist in given command.", {
-          cause: `${name} not found on command: ${interaction.commandName}.`
+          cause: `subcommand: \`${name}\` not found on command: ${ctx.interaction.commandName}.`
         });
         //TODO: return state to command rather than error
         return controller.stop();
       } else {
         const each = permsToString(perms);
         const { needAllPerms } = opts;
-        const memberPermissions = member.permissionsIn(interaction.channel as TextChannel);
+        const memberPermissions = member.permissionsIn(ctx.channel as TextChannel);
         if (needAllPerms === true) {
           if (!memberPermissions.has(perms)) {
-            await interaction.reply({
+            await ctx.reply({
               content:
                 opts.response ??
                 `You are required to have all of the following permissions to run this subcommand in this channel:\n${each}`,
@@ -86,7 +85,7 @@ export function subcommandPermCheck(opts: Options) {
           }
         } else {
           if (!memberPermissions.any(perms)) {
-            await interaction.reply({
+            await ctx.reply({
               content:
                 opts.response ??
                 `You are required to have at least one of the following permissions to run this subcommand in this channel:\n${each}`,
